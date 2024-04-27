5f2d2ebcb529f92ef178c0d7a6d4a86a71226609
Wei <>
2024-04-27 14:45:02 +0800

    init project

---
+++ SwipePlay/SwipePlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwipePlay
+//
+//  Created by Wei Lu on 2024/4/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwipePlay/SwipePlay/SwipePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipePlayApp.swift
+//  SwipePlay
+//
+//  Created by Wei Lu on 2024/4/27.
+//
+
+import SwiftUI
+
+@main
+struct SwipePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
